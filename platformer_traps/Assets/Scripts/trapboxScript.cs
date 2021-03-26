f4ca176eaec00899773d370c2992ee660ec40c2e
holden68 <57766023+holden68@users.noreply.github.com>
2021-03-26 16:05:25 -0500

    made a projectile
    made a projectile

---
+++ platformer_traps/Assets/Scripts/trapboxScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapboxScript : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    Vector2 left = new Vector2(-1, 0);
+    float elapsed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= 4f)
+        {
+            Launch();
+        }
+
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, left * .05f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(left, 300);
+    }
+}
